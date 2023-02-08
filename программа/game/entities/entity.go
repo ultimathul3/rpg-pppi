4bbe3021da20d9f45d11a873b239582676167d44
ultimathul3 <ultimathul3@ya.ru>
2023-02-08 20:54:25 +0300

    Добавлена структура Entity

---
+++ программа/game/entities/entity.go
@@ -0,0 +1,11 @@
+package entities
+
+type Entity struct {
+	HP            int
+	MP            int
+	X, Y          int
+	Width, Height int
+	Speed         int
+	Damage        int
+	Armor         int
+}
