7f92974dc3b5f1569368568d2c1304821f37e015
ultimathul3 <ultimathul3@ya.ru>
2023-02-11 22:47:52 +0300

    Добавлена пара примеров докблоков

--- программа/game/saves.go
+++ программа/game/saves.go
@@ -1,12 +1,28 @@
 package game
 
+/**
+ * Сохранения
+ *
+ * Структура для реализации системы
+ * сохранения/загрузки игрового прогресса.
+ *
+ * @author		Vadim Oleynik
+ * @version		1.0.0
+ * @copyright	MIT License
+ */
 type Saves struct {
 }
 
+/**
+ * Метод для сохранения игрового процесса
+ */
 func (s *Saves) Save() {
 
 }
 
+/**
+ * Метод для загрузки игрового процесса
+ */
 func (s *Saves) Load() {
 
 }
